8c9862aab8afb881ce749fc52cef1f42f9d7683d
ayberkuman <ayberkuman1@gmail.com>
2022-11-10 17:09:10 +0300

    starter template

---
+++ components/common/Button/Button.tsx
@@ -0,0 +1,5 @@
+export const Button = () => {
+  return (
+    <button className="bg-neutral-800 text-accent-purple">click me</button>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,13 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import { Carousel } from "../components/Carousel";
-import styles from "../styles/Home.module.css";
-
-const Home: NextPage = () => {
+export default function Home() {
   return (
-    <div className="">
-      <Carousel />
+    <div className="container flex items-center p-4 mx-auto min-h-screen justify-center">
+      <main>
+        <h1 className="font-mono text-xl code">
+          Welcome to <span className="text-purple-700">Nextjs</span>,{" "}
+          <span className="text-indigo-700">TailwindCSS</span> and{" "}
+          <span className="text-gray-700">TypeScript</span>
+        </h1>
+      </main>
     </div>
   );
-};
-
-export default Home;
+}
